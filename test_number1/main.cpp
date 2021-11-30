70fc9696a80316d6451234a0cdd95ed650ae7d3c
Yashar Balazadegan Sarvrood <yashar.balazadegan@gmail.com>
2021-11-30 16:31:06 -0700

    second commit

--- test_number1/main.cpp
+++ test_number1/main.cpp
@@ -2,7 +2,9 @@
 #include<Eigen/Dense>
 #include"test.h"
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
-    test obj(3,4);
+    std::cout << "Hello vv, world!\n";
+    test obj(3,8);
     std::cout<<obj.add()<<std::endl;
+        std::cout<<obj.add()<<std::endl;
+
 }
--- test_number1/test.h
+++ test_number1/test.h
@@ -6,7 +6,10 @@ class test
 
     private:
     Eigen::Matrix2cd a;
+    Eigen::Matrix2cd b;
+
 int x,y;
+double z;
 public:
 test(int x, int y);
 int add();
