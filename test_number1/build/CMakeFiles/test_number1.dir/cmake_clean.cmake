528a9df08ad8190e617da30c8e9cb5e8d6cfa4a8
Yashar Balazadegan Sarvrood <yashar.balazadegan@gmail.com>
2021-11-30 17:57:10 -0700

    test

---
+++ test_number1/build/CMakeFiles/test_number1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_number1.dir/main.cpp.o"
+  "CMakeFiles/test_number1.dir/main.cpp.o.d"
+  "CMakeFiles/test_number1.dir/test.cpp.o"
+  "CMakeFiles/test_number1.dir/test.cpp.o.d"
+  "test_number1"
+  "test_number1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_number1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
